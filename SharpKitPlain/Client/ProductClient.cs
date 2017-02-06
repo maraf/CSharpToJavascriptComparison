5787a686f0ee485b2a40c89003e88c57cacd291a
Marek Fišera <mara@neptuo.com>
2017-02-06 17:50:37 +0100

    Add SharpKitPlain, but without integration of SharpKit.

---
+++ SharpKitPlain/Client/ProductClient.cs
@@ -0,0 +1,20 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class ProductClient
+    {
+        public void GetList(Action<List<ProductModel>> onSuccess)
+        {
+        }
+
+        public void Get(int id, Action<ProductModel> onSuccess)
+        {
+        }
+    }
+}
