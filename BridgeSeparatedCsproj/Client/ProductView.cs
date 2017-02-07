683d8e08b905e994107c1d8818f6bdb088a5c7ce
Marek Fišera <mara@neptuo.com>
2017-02-07 11:12:28 +0100

    Uncomment ProductView.

--- BridgeSeparatedCsproj/Client/ProductView.cs
+++ BridgeSeparatedCsproj/Client/ProductView.cs
@@ -4,28 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Shared;
+using Bridge.jQuery2;
 
 namespace Client
 {
     public class ProductView
     {
-        //private jQuery root;
+        private jQuery root;
 
-        //public ProductView(jQuery root)
-        //{
-        //    this.root = root;
-        //}
+        public ProductView(jQuery root)
+        {
+            this.root = root;
+        }
 
-        //public void Render()
-        //{
-        //    root.html("Loading...");
-        //    ProductClient client = new ProductClient();
-        //    client.GetList(OnListLoaded);
-        //}
+        public void Render()
+        {
+            root.Html("Loading...");
+            ProductClient client = new ProductClient();
+            client.GetList(OnListLoaded);
+        }
 
-        //private void OnListLoaded(List<ProductModel> models)
-        //{
-        //    root.html("Product count: " + models.Count);
-        //}
+        private void OnListLoaded(List<ProductModel> models)
+        {
+            root.Html("Product count: " + models.Count);
+        }
     }
 }
