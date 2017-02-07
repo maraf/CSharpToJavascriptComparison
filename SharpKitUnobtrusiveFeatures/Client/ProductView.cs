5df0ed625d9ad52bd0b035c26ee7126f92a7cd71
Marek Fišera <mara@neptuo.com>
2017-02-07 11:11:50 +0100

    Add unobstrusive version for SharpKit.

---
+++ SharpKitUnobtrusiveFeatures/Client/ProductView.cs
@@ -0,0 +1,32 @@
+﻿using SharpKit.jQuery;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared;
+
+namespace Client
+{
+    public class ProductView
+    {
+        private jQuery root;
+
+        public ProductView(jQuery root)
+        {
+            this.root = root;
+        }
+
+        public void Render()
+        {
+            root.html("Loading...");
+            ProductClient client = new ProductClient();
+            client.GetList(OnListLoaded);
+        }
+
+        private void OnListLoaded(List<ProductModel> models)
+        {
+            root.html("Product count: " + models.Count);
+        }
+    }
+}
