449e85883e63e74a157efbc7be1b844526d79f2c
Marek Fišera <mara@neptuo.com>
2017-02-07 11:12:18 +0100

    Initial version of Bridge.

---
+++ BridgeSeparatedCsproj/Server/ProductWebService.cs
@@ -0,0 +1,41 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class ProductWebService
+    {
+        public List<ProductModel> GetList()
+        {
+            return new List<ProductModel>() 
+            {
+                new ProductModel() 
+                {
+                    Id = 1,
+                    Name = "Product A",
+                    Price = 100
+                },
+                new ProductModel() 
+                {
+                    Id = 2,
+                    Name = "Product B",
+                    Price = 200
+                }
+            };
+        }
+
+        public ProductModel Get(int id)
+        {
+            return new ProductModel()
+            {
+                Id = id,
+                Name = "Product A",
+                Price = 100
+            };
+        }
+    }
+}
